8381bc92e784544a7d1371b77a9fe1b408bffc26
Steve Severance <sseverance@alphaheavy.com>
2015-04-22 12:51:30 -0700

    better session support

--- src/Network/Consul.hs
+++ src/Network/Consul.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -15,6 +16,7 @@ module Network.Consul (
   , putKeyAcquireLock
   , putKeyReleaseLock
   , withManagedSession
+  , withSession
   , Consistency(..)
   , ConsulClient(..)
   , Datacenter(..)
@@ -24,8 +26,11 @@ module Network.Consul (
   , Session(..)
 ) where
 
-import Control.Concurrent
+import Control.Concurrent hiding (killThread)
+import Control.Concurrent.Lifted (fork, killThread)
+import Control.Concurrent.STM
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Control
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Read as TR
@@ -76,6 +81,38 @@ deleteKey _client@ConsulClient{..} key = I.deleteKey ccManager ccHostname ccPort
 getSessionInfo :: MonadIO m => ConsulClient -> Text -> Maybe Datacenter -> m (Maybe [SessionInfo])
 getSessionInfo _client@ConsulClient{..} = I.getSessionInfo ccManager ccHostname ccPort
 
+withSession :: forall a m. (MonadIO m,MonadBaseControl IO m) => ConsulClient -> Session -> m a -> m a -> m a
+withSession client session action lostAction = do
+  var <- liftIO $ newEmptyTMVarIO
+  tidVar <- liftIO $ newEmptyTMVarIO
+  stid <- fork $ runThread var tidVar
+  tid <- fork $ action >>= \ x -> liftIO $ atomically $ putTMVar var x
+  liftIO $ atomically $ putTMVar tidVar tid
+  ret <- liftIO $ atomically $ takeTMVar var
+  killThread stid
+  return ret
+  where
+    runThread :: TMVar a -> TMVar ThreadId -> m ()
+    runThread var threadVar = do
+      liftIO $ threadDelay (10 * 1000000)
+      x <- getSessionInfo client (sId session) Nothing
+      case x of
+        Just [] -> cancel var threadVar
+        Nothing -> cancel var threadVar
+        Just _ -> runThread var threadVar
+
+    cancel :: TMVar a -> TMVar ThreadId -> m ()
+    cancel resultVar tidVar = do
+      tid <- liftIO $ atomically $ readTMVar tidVar
+      killThread tid
+      empty <- liftIO $ atomically $ isEmptyTMVar resultVar
+      if not empty then do
+        result <- lostAction
+        liftIO $ atomically $ putTMVar resultVar result
+        return ()
+        else return ()
+        
+
 {- Helper Functions -}
 
 {- ManagedSession is a session with an associated TTL healthcheck so the session will be terminated if the client dies. The healthcheck will be automatically updated. -}
@@ -84,12 +121,12 @@ data ManagedSession = ManagedSession{
   msThreadId :: ThreadId
 }
 
-withManagedSession :: MonadIO m => ConsulClient -> Text -> (Session -> m ()) -> m ()
-withManagedSession client ttl action = do
+withManagedSession :: MonadIO m => ConsulClient -> Text -> (Session -> m ()) -> m () -> m ()
+withManagedSession client ttl action lostAction = do
   x <- createManagedSession client Nothing ttl
   case x of
     Just s -> action (msSession s) >> destroyManagedSession client s
-    Nothing -> return ()
+    Nothing -> lostAction >> return ()
 
 createManagedSession :: MonadIO m => ConsulClient -> Maybe Text -> Text -> m (Maybe ManagedSession)
 createManagedSession _client@ConsulClient{..} name ttl = do
@@ -100,14 +137,16 @@ createManagedSession _client@ConsulClient{..} name ttl = do
     f x = do
       tid <- liftIO $ forkIO $ runThread x
       return $ ManagedSession x tid
-
+    
     saneTtl = let Right (x,_) = TR.decimal $ T.filter (/= 's') ttl in x
 
     runThread :: Session -> IO ()
     runThread s = do
-      threadDelay (saneTtl - (saneTtl - 10))
-      I.renewSession ccManager ccHostname ccPort s Nothing
-      return ()
+      threadDelay $ (saneTtl - (saneTtl - 10)) * 1000000
+      x <- I.renewSession ccManager ccHostname ccPort s Nothing
+      case x of
+        True -> runThread s
+        False -> return ()
 
 destroyManagedSession :: MonadIO m => ConsulClient -> ManagedSession -> m ()
 destroyManagedSession _client@ConsulClient{..} (ManagedSession session tid) = do
