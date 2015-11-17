585efdc2df76d6d828f941a0511a629edd46dae6
Chris Heller <hellertime@gmail.com>
2015-11-17 07:11:23 -0500

    Expose deregisterService call

--- src/Network/Consul.hs
+++ src/Network/Consul.hs
@@ -7,6 +7,7 @@ module Network.Consul (
     createSession
   , deleteKey
   , destroySession
+  , deregisterService
   , getKey
   , getKeys
   , getSelf
@@ -110,6 +111,9 @@ getServices _client@ConsulClient{..} = I.getServices ccManager (I.hostWithScheme
 {- Agent -}
 getSelf :: MonadIO m => ConsulClient -> m (Maybe Self)
 getSelf _client@ConsulClient{..} = I.getSelf ccManager (I.hostWithScheme _client) ccPort
+
+deregisterService :: MonadIO m => ConsulClient -> Text -> m ()
+deregisterService _client@ConsulClient{..} = I.deregisterService ccManager (I.hostWithScheme _client) ccPort 
 
 registerService :: MonadIO m => ConsulClient -> RegisterService -> Maybe Datacenter -> m Bool
 registerService _client@ConsulClient{..} = I.registerService ccManager (I.hostWithScheme _client) ccPort
