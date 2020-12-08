445ff7c3ccb43fb7d67983e1ab7e1560264125f8
Niklas Hambüchen <mail@nh2.me>
2020-12-08 10:42:19 +0100

    nix: Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,13 +1,24 @@
-with (import <nixpkgs> {});
 let
+  # Pin nixpkgs.
+  # mpickering's NUR used below currently requires 20.03,
+  # see https://github.com/mpickering/old-ghc-nix/issues/8.
+  pkgs = import (builtins.fetchTarball {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-20.03-2020-12-08";
+    # Current commit from https://github.com/NixOS/nixpkgs/tree/nixos-20.03
+    url = "https://github.com/nixos/nixpkgs/archive/030e2ce817c8e83824fb897843ff70a15c131b96.tar.gz";
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "110kgp4x5bx44rgw55ngyhayr4s19xwy19n6qw9g01hvhdisilwf";
+  }) {};
+
   # Needs NUR from https://github.com/nix-community/NUR
-  ghc = nur.repos.mpickering.ghc.ghc865; # Keep in sync with the GHC version defined by stack.yaml!
+  ghc = pkgs.nur.repos.mpickering.ghc.ghc865; # Keep in sync with the GHC version defined by stack.yaml!
 in
-  haskell.lib.buildStackProject {
+  pkgs.haskell.lib.buildStackProject {
     inherit ghc;
     name = "myEnv";
 
-    nativeBuildInputs = [
+    nativeBuildInputs = with pkgs; [
       # Put libraries required to build here.
       zlib
 
