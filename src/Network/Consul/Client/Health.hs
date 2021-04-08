c79a55137b8693dfa96da0975e90f3281c313ebc
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:23:32 +0000

    Import shuffle, reduce code duplication
    There is still the issue of Text/ByteString/Vector/HashMap collisions on
    some function names, but using `import qualified` to work around that
    for now.

--- src/Network/Consul/Client/Health.hs
+++ src/Network/Consul/Client/Health.hs
@@ -14,31 +14,10 @@ module Network.Consul.Client.Health
   , warnHealthCheck
   ) where
 
-import Control.Concurrent hiding (killThread)
-import Control.Monad (forever)
-import Control.Monad.IO.Class
-import Control.Monad.Catch (MonadMask)
-import Control.Retry
-import Data.Aeson (Value(..), decode,encode)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.HashMap.Strict as H
-import Data.Maybe (catMaybes, isJust, listToMaybe)
-import Data.Monoid ((<>))
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Read as TR
-import Data.Word
-import qualified Data.Vector as V
-import qualified Network.Consul.Internal as I
-import Network.Consul.Types
-import Network.HTTP.Client -- (method, Manager, responseBody)
-import Network.HTTP.Client.TLS (newTlsManager, newTlsManagerWith, tlsManagerSettings)
-import Network.HTTP.Types
-import Network.Socket (PortNumber)
-import UnliftIO (MonadUnliftIO, async, cancel, finally, wait, waitAnyCancel, withAsync)
-
-import Network.Consul.Internal
+import Import
+import qualified Data.ByteString as B (concat) 
+import qualified Data.ByteString.Lazy as BL (toStrict, fromStrict)
+import qualified Data.Text as T (concat, empty, pack, intercalate, unpack)
 
 -- | TODO: Document
 {-getHealthChecks :: MonadIO m => Manager -> Text -> PortNumber -> Maybe Datacenter -> m [Check]
--- src/Network/Consul/Client/KVStore.hs
+++ src/Network/Consul/Client/KVStore.hs
@@ -14,32 +14,10 @@ module Network.Consul.Client.KVStore
   , putKeyReleaseLock
   ) where
 
-import Control.Concurrent hiding (killThread)
-import Control.Monad (forever)
-import Control.Monad.IO.Class
-import Control.Monad.Catch (MonadMask)
-import Control.Retry
-import Data.Aeson (Value(..), decode,encode)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.HashMap.Strict as H
-import Data.Maybe (catMaybes, isJust, listToMaybe)
-import Data.Monoid ((<>))
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Read as TR
-import Data.Word
-import qualified Data.Vector as V
-import qualified Network.Consul.Internal as I
-import Network.Consul.Types
-import Network.HTTP.Client -- (method, Manager, responseBody)
-import Network.HTTP.Client.TLS (newTlsManager, newTlsManagerWith, tlsManagerSettings)
-import Network.HTTP.Types
-import Network.Socket (PortNumber)
-import UnliftIO (MonadUnliftIO, async, cancel, finally, wait, waitAnyCancel, withAsync)
-
-import Network.Consul.Internal
-
+import Import
+import qualified Data.ByteString as B (concat) 
+import qualified Data.ByteString.Lazy as BL (toStrict, fromStrict)
+import qualified Data.Text as T (concat, empty, pack, intercalate, unpack)
  
 -- | TODO: Document!
 deleteKey :: MonadIO m => ConsulClient -> Text -> Bool -> m Bool
@@ -151,7 +129,7 @@ putKey _client@ConsulClient{..} putRequest = do
   liftIO $ withResponse initReq ccManager $ \ response -> do
     bodyParts <- brConsume $ responseBody response
     let body = B.concat bodyParts
-    let result = I.decodeAndStrip body
+    let result = decodeAndStrip body
     case result of
       "true" -> return True
       "false" -> return False
