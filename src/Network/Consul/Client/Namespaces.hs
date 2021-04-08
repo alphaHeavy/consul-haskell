65c3ce933658beb3130527a52d2e7f849a56a1b3
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:46:36 +0000

    add placeholders for Namespace Client API functions

--- src/Network/Consul/Client/Namespaces.hs
+++ src/Network/Consul/Client/Namespaces.hs
@@ -4,8 +4,34 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/namespaces
 module Network.Consul.Client.Namespaces
-  ( 
+  ( createNamespace
+  , deleteNamespace
+  , getNamespace     -- readNamespace
+  , listNamespaces
+  , updateNamespace
   ) where
 
 import Import
+
+-- | TODO: Document
+createNamespace :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+createNamespace = undefined
+
+-- | TODO: Document
+deleteNamespace :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+deleteNamespace = undefined
+
+-- | TODO: Document
+getNamespace :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+getNamespace = undefined
+
+-- | TODO: Document
+listNamespaces :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+listNamespaces = undefined
+
+-- | TODO: Document
+updateNamespace :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+updateNamespace = undefined
+ 
