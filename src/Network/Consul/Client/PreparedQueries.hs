2bbf392dd9d9fab3e7145cd5cfae4b81e7836860
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:46:49 +0000

    add placeholders for Prepared Query Client API functions

--- src/Network/Consul/Client/PreparedQueries.hs
+++ src/Network/Consul/Client/PreparedQueries.hs
@@ -4,8 +4,43 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/query
 module Network.Consul.Client.PreparedQueries
-  ( 
+  ( createQuery
+  , deleteQuery
+  , executeQuery
+  , explainQuery
+  , getQuery
+  , listQueries
+  , updateQuery
   ) where
 
 import Import
+
+-- | TODO: Document
+createQuery :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+createQuery = undefined
+
+-- | TODO: Document
+deleteQuery :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+deleteQuery = undefined
+
+-- | TODO: Document
+executeQuery :: MonadIO m => ConsulClient -> m ()
+executeQuery = undefined
+
+-- | TODO: Document
+explainQuery :: MonadIO m => ConsulClient -> m ()
+explainQuery = undefined
+
+-- | TODO: Document
+getQuery :: MonadIO m => ConsulClient -> m ()
+getQuery = undefined
+
+-- | TODO: Document
+listQueries :: MonadIO m => ConsulClient -> m ()
+listQueries = undefined
+
+-- | TODO: Document
+updateQuery :: MonadIO m => ConsulClient -> m ()
+updateQuery = undefined
