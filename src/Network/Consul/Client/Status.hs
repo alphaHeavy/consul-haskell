f464c84095b2877cea671478e411384e8633046e
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:47:03 +0000

    add placeholders for Status Client API functions

--- src/Network/Consul/Client/Status.hs
+++ src/Network/Consul/Client/Status.hs
@@ -5,7 +5,18 @@
 
 -- | TODO: document module
 module Network.Consul.Client.Status
-  ( 
+  ( getRaftPeer
+  , listRaftPeers
   ) where
 
 import Import
+
+-- | Get Raft Peer
+--getRaftPeer :: MonadIO m => ConsulClient -> Maybe Datacenter -> m (Maybe Peer)
+getRaftPeer :: MonadIO m => ConsulClient -> m ()
+getRaftPeer = undefined
+
+-- | List Raft Peers
+--listRaftPeers :: MonadIO m => ConsulClient -> Maybe Datacenter -> m ([Peer])
+listRaftPeers :: MonadIO m => ConsulClient -> m ()
+listRaftPeers = undefined
