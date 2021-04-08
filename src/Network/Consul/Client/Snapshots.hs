9ad8b1972ef8882fe3bf941ce9dd90b2b3667a01
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:46:57 +0000

    add placeholders for Snapshot Client API functions

--- src/Network/Consul/Client/Snapshots.hs
+++ src/Network/Consul/Client/Snapshots.hs
@@ -4,8 +4,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/snapshot
 module Network.Consul.Client.Snapshots
-  ( 
+  ( createSnapshot -- generateSnapshot
+  , restoreSnapshot
   ) where
 
 import Import
+
+-- | TODO: Document
+createSnapshot :: MonadIO m => ConsulClient -> m ()
+createSnapshot = undefined
+
+-- | TODO: Document
+restoreSnapshot :: MonadIO m => ConsulClient -> m ()
+restoreSnapshot = undefined
