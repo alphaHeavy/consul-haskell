6318863d2ad7cfb8bee41f9de6cad67b9b0f22a9
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 14:47:11 +0000

    add placeholders for Transaction Client API functions

--- src/Network/Consul/Client/Transactions.hs
+++ src/Network/Consul/Client/Transactions.hs
@@ -4,8 +4,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/txn
 module Network.Consul.Client.Transactions
-  ( 
+  ( createTransaction 
   ) where
 
 import Import
+
+-- | Create Transaction
+--createTransaction :: MonadIO m => ConsulClient -> m (Maybe Transaction)
+createTransaction :: MonadIO m => ConsulClient -> m ()
+createTransaction = undefined
