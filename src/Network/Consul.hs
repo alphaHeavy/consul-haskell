543ddf1423921e2167cd371692b94c74ba82b8a7
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-08 05:40:34 +0000

    add empty source files for client API functions
    The nice thing is that with the imports in Network.Consul, the empty
    source files have a GHC warning pointed at them.

--- src/Network/Consul.hs
+++ src/Network/Consul.hs
@@ -53,12 +53,22 @@ import Import
 
 import Network.Consul.Types
 -- Consul Client APIs
+import Network.Consul.Client.Acl
 import Network.Consul.Client.Agent
 import Network.Consul.Client.Catalog
+import Network.Consul.Client.Config
+import Network.Consul.Client.Connect
+import Network.Consul.Client.Events
 import Network.Consul.Client.Health
 import Network.Consul.Client.Init
 import Network.Consul.Client.KVStore
+import Network.Consul.Client.Namespaces
+import Network.Consul.Client.Operator
+import Network.Consul.Client.PreparedQueries
 import Network.Consul.Client.Session
+import Network.Consul.Client.Snapshots
+import Network.Consul.Client.Status
+import Network.Consul.Client.Transactions
 
 import Network.Consul.Sequencer
 import Network.Consul.Misc
---
+++ src/Network/Consul/Client/Transactions.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- | TODO: document module
+module Network.Consul.Client.Transactions
+  ( 
+  ) where
+
+
