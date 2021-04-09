e5fb92466f59ab71c6f3e6b218f0998443bfa1cd
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-09 02:35:03 +0000

    add placeholders for Event Client API functions

--- src/Network/Consul/Client/Events.hs
+++ src/Network/Consul/Client/Events.hs
@@ -5,7 +5,17 @@
 
 -- | TODO: document module
 module Network.Consul.Client.Events
-  ( 
+  ( createEvent
+  , listEvents
   ) where
 
+import Import
 
+-- | TODO: Document
+createEvent :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+createEvent = undefined
+
+
+-- | TODO: Document
+listEvents :: MonadIO m => ConsulClient -> SessionRequest -> m ()
+listEvents = undefined
