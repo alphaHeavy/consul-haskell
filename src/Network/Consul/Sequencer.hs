daa2a00c93c12b2610393026479d1cee4fa7be78
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-09 02:06:46 +0000

    fixup imports on Consul.Sequencer

--- src/Network/Consul/Sequencer.hs
+++ src/Network/Consul/Sequencer.hs
@@ -10,37 +10,13 @@ module Network.Consul.Sequencer
   , withSequencer
   ) where
 
-import Control.Concurrent hiding (killThread)
-import Control.Monad (forever)
-import Control.Monad.IO.Class
-import Control.Monad.Catch (MonadMask)
-import Control.Retry
-import Data.Aeson (Value(..), decode,encode)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.HashMap.Strict as H
-import Data.Maybe (catMaybes, isJust, listToMaybe)
-import Data.Monoid ((<>))
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.Read as TR
-import Data.Word
-import qualified Data.Vector as V
-import qualified Network.Consul.Internal as I
-import Network.Consul.Types
-import Network.HTTP.Client -- (method, Manager, responseBody)
-import Network.HTTP.Client.TLS (newTlsManager, newTlsManagerWith, tlsManagerSettings)
-import Network.HTTP.Types
-import Network.Socket (PortNumber)
-import UnliftIO (MonadUnliftIO, async, cancel, finally, wait, waitAnyCancel, withAsync)
+import Import
 
-import Network.Consul.Internal
 import Network.Consul.Client.KVStore
 
 -- | TODO: Document
 getSequencerForLock :: MonadIO m => ConsulClient -> Text -> Session -> m (Maybe Sequencer)
 getSequencerForLock client key session = do
-  let dc = ccDatacenter client
   kv <- getKey client key Nothing (Just Consistent)
   case kv of
     Just k -> do
