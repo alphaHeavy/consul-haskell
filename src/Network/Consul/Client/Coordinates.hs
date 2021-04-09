b639b02555497dda6079b3c76e73290ebd73c506
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-09 02:55:13 +0000

    Add placeholder functions for Coordinates client API

--- src/Network/Consul/Client/Coordinates.hs
+++ src/Network/Consul/Client/Coordinates.hs
@@ -4,8 +4,33 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/coordinate
 module Network.Consul.Client.Acl
-  ( 
+  ( getNodeLANCoordinates
+  , listNodeLANCoordinates
+  , listServerWANCoordinates
+  , updateNodeLANCoordinates
   ) where
 
 import Import
+
+-- | Read LAN Coordinates for a node
+getNodeLANCoordinates :: MonadIO m => ConsulClient -> m ()
+getNodeLANCoordinates = undefined
+
+-- | Read LAN Coordinates for all nodes
+listNodeLANCoordinates :: MonadIO m => ConsulClient -> m ()
+listNodeLANCoordinates = undefined
+
+-- | Read WAN Coordinates
+-- > This endpoint returns the WAN network coordinates for all Consul servers,
+-- > organized by datacenters. It serves data out of the server's local Serf data,
+-- > so its results may vary as requests are handled by different servers in the
+-- > cluster.
+listServerWANCoordinates :: MonadIO m => ConsulClient -> m ()
+listServerWANCoordinates = undefined
+
+-- | Update LAN Coordinates for a node
+updateNodeLANCoordinates :: MonadIO m => ConsulClient -> m ()
+updateNodeLANCoordinates = undefined
+
