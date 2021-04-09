86a3052da21adc7231f364bca98b4b8343a5d456
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-09 02:54:51 +0000

    add placeholder functions for Config client API

--- src/Network/Consul/Client/Config.hs
+++ src/Network/Consul/Client/Config.hs
@@ -4,8 +4,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- | TODO: document module
+-- https://www.consul.io/api-docs/config
 module Network.Consul.Client.Config
-  ( 
+  ( putConfig
+  , getConfig
   ) where
 
 import Import
+
+-- | Apply (Put) Configuration
+putConfig :: MonadIO m => ConsulClient -> m ()
+putConfig = undefined
+
+-- | Get Configuration
+getConfig :: MonadIO m => ConsulClient -> m ()
+getConfig = undefined
