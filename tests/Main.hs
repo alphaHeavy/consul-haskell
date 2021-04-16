0e5819985808fa49accd624a8570c30af986f30c
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2021-04-16 03:53:07 +0000

    code shuffle for tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -35,7 +35,6 @@ import Network.Consul
 import Network.Consul.Types
 import Network.Consul
 import Network.Consul.Internal (emptyHttpManager)
-import Network.Socket (PortNumber)
 import System.IO (hFlush)
 import System.Process.Typed (proc)
 import qualified System.Process.Typed as PT
@@ -43,55 +42,10 @@ import System.Random
 import System.Timeout (timeout)
 import Test.Tasty
 import Test.Tasty.HUnit
-import UnliftIO.Temporary (withSystemTempFile)
 
-import SocketUtils (isPortOpen, simpleSockAddr)
+import Util
 
 -- * Internal Helper Utilities
-
--- 5 seconds.
-fiveSecondMicros :: Int
-fiveSecondMicros = 5 * 1000 * 1000
-
--- Name of existing health check we can rely on for tests that use the Session API
-serfHealth :: Text
-serfHealth = "serfHealth"
-
--- list of names of service/node checks (which should exist in consul already)
-checkIds :: [Text]
-checkIds = [serfHealth]
-
--- for requests to session API
-lockDelay :: Maybe a
-lockDelay = Nothing
-
--- Sleep for N seconds with `threadDelay()`.
-sleep :: Double -> IO ()
-sleep seconds = threadDelay (ceiling (seconds * 1e6))
-
--- Define a `consulHost` for use in running tests against the Consul Agent
-localhost :: ConsulHost
-localhost = "localhost"
-
--- The IP Address of the local agent.
-localNodeAddr :: Text
-localNodeAddr = "127.0.0.1"
-
--- Instantiate a `ConsulHost` for these tests.
-localNode :: Node
-localNode = Node localhost localNodeAddr
-
--- The network port where the Consul Agent will listen for the HTTP API.
-consulPort :: PortNumber
-consulPort = 18500
-
-dc1 :: Maybe Datacenter
-dc1 = Just $ Datacenter "dc1"
-
--- Initialize a new `ConsulClient`.
-newClient :: IO ConsulClient
-newClient = initializeConsulClient localhost consulPort emptyHttpManager
-
 {- Internal Tests -}
 internalKVTests :: TestTree
 internalKVTests =
@@ -540,25 +494,6 @@ allTests =
     , runServiceTests
     , sequencerTests
     ]
-
--- Backwards compatible `withProcessTerm`.
-withProcessTerm :: PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> IO a) -> IO a
-#if MIN_VERSION_typed_process(0,2,5)
-withProcessTerm = PT.withProcessTerm
-#else
-withProcessTerm = PT.withProcess
-#endif
-
-waitForConsulOrFail :: IO ()
-waitForConsulOrFail = do
-  success <-
-    retrying
-      (constantDelay 50000 <> limitRetries 100) -- 100 times, 50 ms each
-      (\_status isOpen -> return (not isOpen)) -- when to retry
-      $ \_status -> do
-        isPortOpen $ (simpleSockAddr (127,0,0,1) consulPort)
-  when (not success) $ do
-    error $ "Could not connect to Consul within reasonable time"
 
 main :: IO ()
 main = do
