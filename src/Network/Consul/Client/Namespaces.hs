c36361c16b0f33026ea732cf8b44d556ffb0bc2f
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2022-08-14 03:42:06 +0000

    Network/Consul: drop unused LANGUAGE pragmas
    statisfy hlint

--- src/Network/Consul/Client/Namespaces.hs
+++ src/Network/Consul/Client/Namespaces.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 
--- src/Network/Consul/Misc.hs
+++ src/Network/Consul/Misc.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
