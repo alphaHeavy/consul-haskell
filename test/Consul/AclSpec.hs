6b0412522066a612c1e37cceb55e074ca0c8223b
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2022-08-22 03:31:15 +0000

    test/Consul/AclSpec: add testsuiteSettingsWithAclsEnabled and use it with consulServerSetupFuncWith
    when we run ACL tests, we have special/custom settings enabled.

--- test/Consul/AclSpec.hs
+++ test/Consul/AclSpec.hs
@@ -5,9 +5,19 @@ module Consul.AclSpec where
 
 import Import
 
-spec :: Spec
-spec = setupAround consulServerSetupFunc $ do
+testsuiteSettingsWithAclsEnabled = 
+  TestsuiteSettings
+    { displayConsulServerLogs = False -- True
+    , enableAcls = True
+    , verboseLogs = True -- False
+    }
 
+spec :: Spec
+spec = setupAround (consulServerSetupFuncWith testsuiteSettingsWithAclsEnabled) $ do
+
+  -- TODO: why does this operation fail with leader unable to find and remove the acl-reset-bootstrap
+  -- agent.server.acl: failed to remove bootstrap file: error="remove
+  -- /run/user/1001/consul-server-e96ac147c2a743a8/acl-bootstrap-reset: no such file or directory"
   it "BootstrapAcls" $ \consulServerHandle -> do
     client@ConsulClient{..} <- newClient $ consulServerHandleHttpPort consulServerHandle
     -- specify the datacenter as part of our request
