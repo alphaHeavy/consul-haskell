e0a50c4e9657954551e58180130392ea29965a45
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2022-09-03 06:52:48 +0000

    test/Consul/AclPolicySpec: add test for aclPolicyUpdate function
    create a policy, change the name for that policy and update it, then
    compare the result to confirm.

--- test/Consul/AclPolicySpec.hs
+++ test/Consul/AclPolicySpec.hs
@@ -80,3 +80,52 @@ spec = setupAround (consulServerSetupFuncWith testsuiteSettingsWithAclsEnabled) 
                 case result of
                   True -> context "AclPolicyDelete: successful!" $ pure ()
                   False -> expectationFailure ("AclPolicyDelete: failed")
+
+
+  it "AclPolicyUpdate" $ \consulServerHandle -> do
+    client@ConsulClient{..} <- newClient $ consulServerHandleHttpPort consulServerHandle
+    response <- aclBootstrap client{ ccDatacenter = dc1  }
+    case response of
+      Left e -> expectationFailure ("AclPolicyUpdate(Bootstrap acls): failed " ++ e)
+      Right aclBootstrapResponse -> do
+        -- extract token from response
+        let token = consulApiResponseAclBootstrapSecretId aclBootstrapResponse
+        -- create another consul client that is authenticated using the new token
+        secondClient@ConsulClient{..} <-
+          newClientAuth
+            (consulServerHandleHttpPort consulServerHandle)
+            token
+        -- setup a policy record
+        let policyName = "Test-Policy"
+        let updatedPolicyName = "Updated-Policy-Name"
+        let policyRequest =
+              ConsulApiRequestAclPolicyCreate
+                { consulApiRequestAclPolicyCreateName = policyName
+                , consulApiRequestAclPolicyCreateDescription = "A policy for testing"
+                , consulApiRequestAclPolicyCreateRules = "node_prefix \"\" { policy = \"read\"}"
+                , consulApiRequestAclPolicyCreateDatacenters = ["dc1"] -- None is all
+              --, consulApiRequestAclPolicyCreateNamespace = Nothing -- TODO: enterprise-only?
+                }
+        -- use the second (authenticated) client to create acl policy
+        policy <- aclPolicyCreate secondClient { ccDatacenter = dc1 } policyRequest
+        case policy of
+          Left e -> expectationFailure ("AclPolicyUpdate: failed " ++ e)
+          Right aclPolicyResponse -> do
+            let updatedPolicyRequest =
+                  AclPolicy
+                    { aclPolicyId = (aclPolicyId aclPolicyResponse)
+                    , aclPolicyName = updatedPolicyName 
+                    , aclPolicyDescription = (aclPolicyDescription aclPolicyResponse)
+                    , aclPolicyRules = (aclPolicyRules aclPolicyResponse)
+                    , aclPolicyDatacenters = (aclPolicyDatacenters aclPolicyResponse)
+                    , aclPolicyHash = (aclPolicyHash aclPolicyResponse)
+                    , aclPolicyCreateIndex = (aclPolicyCreateIndex aclPolicyResponse)
+                    , aclPolicyModifyIndex = (aclPolicyModifyIndex aclPolicyResponse)
+                    }
+            updatedPolicy <- aclPolicyUpdate secondClient { ccDatacenter = dc1 } updatedPolicyRequest
+            case updatedPolicy of
+              Left e -> expectationFailure ("AclPolicyUpdate: updated policy failed " ++ e)
+              Right updatedPolicyResponse -> do
+                context "AclPolicyUpdate: successful" $ (aclPolicyName updatedPolicyResponse) `shouldBe` updatedPolicyName
+
+
