eb864559aa5016fabd1c28ef090af651cae4f385
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2022-09-05 23:47:29 +0000

    Network/Consul/Client/AclToken: implement the other Read/Update/Delete/List functions
    rename `aclTokenRead` to `aclTokenReadById` and `aclTokenReadSelf`

--- src/Network/Consul/Client/AclToken.hs
+++ src/Network/Consul/Client/AclToken.hs
@@ -14,13 +14,14 @@ module Network.Consul.Client.AclToken
   , aclTokenCreate
   , aclTokenDelete
   , aclTokenList
-  , aclTokenRead
+  , aclTokenReadById
   , aclTokenReadSelf
   , aclTokenUpdate
   ) where
 
 import Import
 
+aclTokenClone = undefined
 
 -- | TODO
 aclTokenCreate
@@ -31,10 +32,58 @@ aclTokenCreate client@ConsulClient{..} token =
   consulHttpPutHelper client "/v1/acl/token" token
 
 
+-- | This endpoint deletes an existing ACL Token.
+--
+aclTokenDelete
+  :: ConsulClient -- ^
+  -> Text -- TODO: UUID
+  -> IO (Either String Bool) -- ^
+aclTokenDelete client@ConsulClient{..} id =  do
+  consulHttpDeleteHelper client ("/v1/acl/token/" <> id)
 
-aclTokenClone = undefined
-aclTokenDelete = undefined
-aclTokenList = undefined
-aclTokenRead = undefined
-aclTokenReadSelf = undefined
-aclTokenUpdate = undefined
+
+-- | This endpoint lists (all?) ACL tokens.
+--
+-- TODO: support ns query parameter (namespace, enterprise-only)
+--
+aclTokenList
+  :: ConsulClient -- ^
+  -> IO (Either String [AclToken]) -- ^
+aclTokenList client@ConsulClient{..} =
+  consulHttpGetHelper client "/v1/acl/tokens"
+
+
+-- | This endpoint reads an existing ACL token with the given Id.
+--
+-- TODO: support ns query parameter (namespace, enterprise-only)
+--
+aclTokenReadById
+  :: ConsulClient -- ^
+  -> Text -- ^ TODO: AccessorId (UUID)
+  -> IO (Either String AclToken) -- ^
+aclTokenReadById client id =  do
+  consulHttpGetHelper client ("/v1/acl/token/" <> id)
+
+-- | This endpoint returns the ACL token details that matches the secret ID
+--   specified with the X-Consul-Token header or the token query parameter.
+--
+-- TODO: support ns query parameter (namespace, enterprise-only)
+--
+aclTokenReadSelf
+  :: ConsulClient -- ^
+  -> IO (Either String AclToken) -- ^
+aclTokenReadSelf client =  do
+  consulHttpGetHelper client "/v1/acl/token/self/"
+
+
+-- | This endpoint updates an existing ACL Token.
+--
+-- TODO: support ns query parameter (namespace, enterprise-only)
+--
+aclTokenUpdate
+  :: ConsulClient -- ^
+  -> AclToken
+  -> IO (Either String AclToken) -- ^
+aclTokenUpdate client token = -- do
+  consulHttpPutHelper client ("/v1/acl/token/" <> (aclTokenAccessorId token)) token
+
